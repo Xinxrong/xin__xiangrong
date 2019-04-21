eb215da6ae26bdd39c680f9a9d3a8bbb271a0754
Xinxrong <1036809143@qq.com>
2019-04-21 21:01:17 +0800

    第八周作业，对不起老师，有一点晚，家里有一点事情。

---
+++ 12.12/include/SalariedEmployee.h
@@ -0,0 +1,23 @@
+#ifndef SALARIEDEMPLOYEE_H
+#define SALARIEDEMPLOYEE_H
+
+#include <string>
+#include "Employee.h"
+class SalariedEmployee:public Employee
+{
+    public:
+        SalariedEmployee(const std::string &,const std::string &,const std::string &,int,int,int,double=0.0);
+        virtual ~SalariedEmployee(){ }
+
+        void setWeeklySalary(double);
+        double getWeeklySalary()const ;
+
+        virtual double earnings()const override;
+        virtual void print ()const override;
+
+
+    private:
+        double weeklySalary;
+};
+
+#endif
